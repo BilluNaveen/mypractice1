506e1e45d34dd74b612e89ad8057006cf98f3b9d
BilluNaveen <95737128+BilluNaveen@users.noreply.github.com>
2021-12-13 12:52:08 +0530

    polls

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-
+import sys
 from rest_framework.response import Response
 
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from .models import Question
 
 
 @api_view()
 def home(request):
-    return Response({'status':200,'message':"hello naveen"})
+    return Response({'status':200,'message':"hello"})
+
+def get_data(Question):
+    post=Question.objects.all()
+    return post
+
+check=get_data(Question)
+print(check)
+
+
 
 # Create your views here.
