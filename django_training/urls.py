65a0653a750a1aa8bb4148c4835304e30e7fcc42
BilluNaveen <95737128+BilluNaveen@users.noreply.github.com>
2021-12-13 19:41:35 +0530

    add some project files also

--- django_training/urls.py
+++ django_training/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from polls import views
 
 urlpatterns = [
-    path('',include('polls.urls')),
+    path('emp/',views.QuestionList.as_view()),
     path('admin/', admin.site.urls),
 ]
