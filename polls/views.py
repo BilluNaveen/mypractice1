6c6d2df774207eea3bf506f7bb469d864b728d84
BilluNaveen <95737128+BilluNaveen@users.noreply.github.com>
2021-12-14 17:19:34 +0530

    authentication

--- polls/views.py
+++ polls/views.py
@@ -10,13 +10,22 @@ from .serializer import questionSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-@api_view()
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import authentication_classes,permission_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
+@api_view(["GET"])
+
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def get_data(request):
     obj=Question.objects.all()
     serializer=questionSerializer(obj,many=True)
     return Response(serializer.data)
 
 @api_view(["POST"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def post_data(request):
     serializer=questionSerializer(data=request.data)
     if serializer.is_valid():
@@ -24,6 +33,8 @@ def post_data(request):
     return Response(serializer.data)
 
 @api_view(["POST"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def update_data(request):
     obj = Question.objects.get(id=id)
     serializer=questionSerializer(instance=obj,data=request.data)
@@ -33,6 +44,8 @@ def update_data(request):
 
 
 @api_view(["DELETE"])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def delete_data(request,id):
     obj = Question.objects.get(id=id)
     obj.delete()
